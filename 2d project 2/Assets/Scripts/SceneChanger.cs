f40e88990a4225c0454d31ad7928999e4fc68a71
KyanPat <kyantsoi1@gmail.com>
2025-02-19 21:15:27 +0100

    beetje extra visuele details

---
+++ 2d project 2/Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class SceneChanger : MonoBehaviour
+{
+    public string sceneName;
+    public TextMeshProUGUI levelText;
+
+    void Start()
+    {
+        levelText.text = sceneName;
+    }
+
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+    public void ExitApplication()
+    {
+        Application.Quit();
+    }
+}
